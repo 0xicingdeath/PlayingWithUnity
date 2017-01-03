abd64db925fdac34bcd4d1128e27c8427518ced3
Natalie Chin <chinnh@mcmaster.ca>
2017-01-03 09:15:10 -0500

    first commit

---
+++ UnityTestProject/Assets/Scripts/Move.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//providing access to Unity Library 
+public class Move : MonoBehaviour 
+{
+	public float speed; 
+	// Use this for initialization -> constructor equipment 
+	void Start () 
+	{
+		speed = 3; 
+		print ("Hello, World!");
+		transform.position = new Vector3 (0, 0, 0);
+	}
+
+	// Update is called once per frame -> equivalence while(true) loop
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.LeftArrow)) 
+		{
+			transform.position -= speed*Time.deltaTime * (new Vector3 (1, 0, 0));
+		} //end of if statement
+
+		if (Input.GetKey(KeyCode.RightArrow)) 
+		{
+			transform.position += speed*Time.deltaTime*(new Vector3 (1, 0, 0));
+		} //end of if statement
+
+		if (Input.GetKey(KeyCode.UpArrow)) 
+		{
+			transform.position += speed*Time.deltaTime*(new Vector3 (0, 1, 0));
+		} //end of if statement
+
+		if (Input.GetKey(KeyCode.DownArrow)) 
+		{
+			transform.position += speed*Time.deltaTime*(new Vector3 (0, -1, 0));
+		} //end of if statement
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			
+		} 
+	}
+}
